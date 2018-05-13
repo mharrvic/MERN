c94d934eaaf1030eda49372a5226ee7c3ad82b33
mak <mharrvic@gmail.com>
2018-05-14 03:16:31 +0800

    Added Modules

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://brad:brad@ds231725.mlab.com:31725/devconnector',
+  mongoURI: 'mongodb://mak:mak@ds151169.mlab.com:51169/thesis',
   secretOrKey: 'secret'
 };
