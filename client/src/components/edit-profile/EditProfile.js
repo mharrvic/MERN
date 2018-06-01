83cb94c33d192b2c54bd576ecf796d676d0d8319
mharrvic <mharrvic@gmail.com>
2018-06-01 15:44:25 +0400

    Add Education Form Functionality

--- client/src/components/edit-profile/EditProfile.js
+++ client/src/components/edit-profile/EditProfile.js
@@ -46,52 +46,60 @@ class CreateProfile extends Component {
 
     // 1st profile = profile state, 2nd profile = object in the state
     if (nextProps.profile.profile) {
-      const profile = nextProps.profile.profile;
+      // const profile = nextProps.profile.profile;
+
+      const { skills, social, ...profile } = nextProps.profile.profile;
+      this.setState(prevState => ({
+        ...prevState,
+        ...profile,
+        ...social,
+        skills: skills.join(','),
+      }));
 
       // Bring skills array back to CSV
-      const skillsCSV = profile.skills.join(',');
+      // const skillsCSV = profile.skills.join(',');
 
       // If profile field doesnt exist, make empty string
-      profile.status = !isEmpty(profile.status) ? profile.status : '';
-      profile.company = !isEmpty(profile.company) ? profile.company : '';
-      profile.website = !isEmpty(profile.website) ? profile.website : '';
-      profile.location = !isEmpty(profile.location) ? profile.location : '';
-      profile.githubusername = !isEmpty(profile.githubusername)
-        ? profile.githubusername
-        : '';
-      profile.bio = !isEmpty(profile.bio) ? profile.bio : '';
-      profile.social = !isEmpty(profile.social) ? profile.social : {};
-      profile.twitter = !isEmpty(profile.social.twitter)
-        ? profile.social.twitter
-        : '';
-      profile.facebook = !isEmpty(profile.social.facebook)
-        ? profile.social.facebook
-        : '';
-      profile.linkedin = !isEmpty(profile.social.linkedin)
-        ? profile.social.linkedin
-        : '';
-      profile.youtube = !isEmpty(profile.social.youtube)
-        ? profile.social.youtube
-        : '';
-      profile.instagram = !isEmpty(profile.social.instagram)
-        ? profile.social.instagram
-        : '';
+      // profile.status = !isEmpty(profile.status) ? profile.status : '';
+      // profile.company = !isEmpty(profile.company) ? profile.company : '';
+      // profile.website = !isEmpty(profile.website) ? profile.website : '';
+      // profile.location = !isEmpty(profile.location) ? profile.location : '';
+      // profile.githubusername = !isEmpty(profile.githubusername)
+      //   ? profile.githubusername
+      //   : '';
+      // profile.bio = !isEmpty(profile.bio) ? profile.bio : '';
+      // profile.social = !isEmpty(profile.social) ? profile.social : {};
+      // profile.twitter = !isEmpty(profile.social.twitter)
+      //   ? profile.social.twitter
+      //   : '';
+      // profile.facebook = !isEmpty(profile.social.facebook)
+      //   ? profile.social.facebook
+      //   : '';
+      // profile.linkedin = !isEmpty(profile.social.linkedin)
+      //   ? profile.social.linkedin
+      //   : '';
+      // profile.youtube = !isEmpty(profile.social.youtube)
+      //   ? profile.social.youtube
+      //   : '';
+      // profile.instagram = !isEmpty(profile.social.instagram)
+      //   ? profile.social.instagram
+      //   : '';
 
       // Set component fields state
-      this.setState({
-        handle: profile.handle,
-        company: profile.company,
-        website: profile.website,
-        location: profile.location,
-        status: profile.status,
-        skills: skillsCSV,
-        githubusername: profile.githubusername,
-        bio: profile.bio,
-        twitter: profile.twitter,
-        facebook: profile.facebook,
-        linkedin: profile.linkedin,
-        youtube: profile.youtube,
-      });
+      // this.setState({
+      //   handle: profile.handle,
+      //   company: profile.company,
+      //   website: profile.website,
+      //   location: profile.location,
+      //   status: profile.status,
+      //   skills: skillsCSV,
+      //   githubusername: profile.githubusername,
+      //   bio: profile.bio,
+      //   twitter: profile.twitter,
+      //   facebook: profile.facebook,
+      //   linkedin: profile.linkedin,
+      //   youtube: profile.youtube,
+      // });
     }
   }
 
