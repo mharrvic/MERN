1bfef8aa513f656e9cf3933cbb9de78c24a27ab0
mharrvic <mharrvic@gmail.com>
2018-06-05 16:48:02 +0400

    Production dummy key

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://mak:mak@ds151169.mlab.com:51169/thesis',
-  secretOrKey: 'secret'
-};
+if (process.env.NOD_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
